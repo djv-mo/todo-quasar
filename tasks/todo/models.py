4ff028965030cba8f39bb1bd9ea3aa7e0eb572d0
mo <mo@mo.com>
2021-06-19 02:10:13 +0200

    adding todo model

--- tasks/todo/models.py
+++ tasks/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=250)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
