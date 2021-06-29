4a6568422001cfd8826ea6258731685f747686ea
mo <mo@mo.com>
2021-06-30 01:29:52 +0200

    adding cors header

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third party APPs
     'rest_framework',
+    'corsheaders',
     # my APPs
     'todo'
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
