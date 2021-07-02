cdbae216a679ed6db96c77a2aa2756ac8edfb878
mo <mo@mo.com>
2021-07-02 15:00:04 +0200

    adding list by todays date to main view

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+
+]
--- tasks/todo/views.py
+++ tasks/todo/views.py
@@ -1,9 +1,12 @@
+from datetime import date
 from rest_framework import viewsets
 from .models import Todo
 from .serializers import TodoSerializer
 
 
 class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all().order_by('-created_at')
+    # today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+    # today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+
+    queryset = Todo.objects.all().order_by('-created_at').filter(created_at__startswith=date.today())
     serializer_class = TodoSerializer
-    
